6ad862c89dace0772d6c85b77987780fb7df1cbe
Thubr <arthur07021999@yahoo.com.br>
2020-01-04 21:43:18 -0300

    Initial project

---
+++ CyberpunkHelper/CyberpunkHelper/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using CyberpunkHelper.Services;
+using CyberpunkHelper.Views;
+
+namespace CyberpunkHelper
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
